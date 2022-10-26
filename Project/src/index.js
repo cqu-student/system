e5b1f098ebad12010d726af0034859eef14295f3
cqu-student <68796008+cqu-student@users.noreply.github.com>
2022-10-26 12:40:06 +0800

    调通

--- Project/src/index.js
+++ Project/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ajax from "./ajax"
-const BASE = '/api'
-export const reqLogin = (username,password) => ajax(BASE + '/login',{username,password},'POST')
+// import ajax from "./ajax"
+// const BASE = '/api'
+// export const reqLogin = (username,password) => ajax(BASE + '/login',{username,password},'POST')
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
