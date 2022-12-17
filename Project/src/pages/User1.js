bff56f5b87d00a83db8d7a0d0065837c92320a71
cqu-student <68796008+cqu-student@users.noreply.github.com>
2022-12-17 16:41:29 +0800

    新界面

--- Project/src/pages/User1.js
+++ Project/src/pages/User1.js
@@ -1,8 +1,27 @@
 import React from "react";
+import "./User1.css";
 function User1(){
     return (
-        <div >
-            <h1>第一个界面</h1>
+        <div className="main" >
+            <div className="Title_text">基于场景理解的图片分割与识别系统</div>
+            <div className="content">
+                <div className="left_content">
+                    <div className="algorithm"><p className="algorithm1">SGAN</p></div>
+                    <div className="algorithm"><p className="algorithm1">Faster R-CNN</p></div>
+                    <div className="algorithm"><p className="algorithm1">YOLO</p></div>
+                    <div className="algorithm"><p className="algorithm1">SSD</p></div>
+                    <div className="algorithm"><p className="algorithm1">算法对比</p></div>
+                </div>
+                <div className="right_content">
+                    <div className="right_content_block1"> 
+                    </div>
+                    <div className="right_content_block2">
+                    </div>
+                    <div className="bottom_box">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
